a3fe0a148ec152cf985ee7b4c35808bb5bbc576e
mozqueda-alejandro <admozqueda@nevada.unr.edu>
2023-07-24 23:54:12 -0700

    Added icons

--- ViewModels/SettingsViewModel.cs
+++ ViewModels/SettingsViewModel.cs
@@ -1,4 +1,6 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Avalonia;
+using Avalonia.Styling;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CustomerDemo.Services;
 
@@ -6,18 +8,23 @@ namespace CustomerDemo.ViewModels;
 
 public partial class SettingsViewModel : ViewModelBase
 {
-    [ObservableProperty]
-    private INavigationService? _navigationService;
-    
-    [RelayCommand]
-    private void NavigateToHome() => NavigationService?.NavigateTo<HomeViewModel>();
+    public ThemeVariant[] AppThemes { get; } =
+        new[] { ThemeVariant.Light, ThemeVariant.Dark};
 
+    [ObservableProperty]
+    private ThemeVariant _currentAppTheme = ThemeVariant.Dark;
+    
+    partial void OnCurrentAppThemeChanged(ThemeVariant? value)
+    {
+        if (Application.Current!.ActualThemeVariant != value)
+        {
+            Application.Current.RequestedThemeVariant = value;
+        }
+    }
+
+    
     public SettingsViewModel()
     {
-    }
-    
-    public SettingsViewModel(INavigationService navigationService)
-    {
-        NavigationService = navigationService;
+        
     }
 }
--- ViewModels/VendorsViewModel.cs
+++ ViewModels/VendorsViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
 
 namespace CustomerDemo.ViewModels;
 
@@ -6,4 +8,14 @@ public partial class VendorsViewModel : ViewModelBase
 {
     [ObservableProperty]
     private string _title = "Binding from VendorsViewModel";
+    
+    public ISeries[] Series { get; set; } 
+        = new ISeries[]
+        {
+            new LineSeries<double>
+            {
+                Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
+                Fill = null
+            }
+        };
 }
